398025b9556a796cb789e15e88810f0207a35524
Jeremy Treutler <Jeremy@fa-divers.com>
2022-01-13 00:59:07 +0100

    add transparent start up window

---
+++ src/ProjectCollection/TransparentRabbitStartUp/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace TransparentRabbitStartUp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
---
+++ src/ProjectCollection/TransparentRabbitStartUp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace TransparentRabbitStartUp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = new Thread(() =>
+            {
+                new MainWindow().ShowDialog();
+            });
+
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+
+            Console.ReadKey();
+        }
+    }
+}
